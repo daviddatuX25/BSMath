5e1508fccba2ea63caa72e21881680f1ae549f3a
David Datu Sarmiento <sarmientodaviddatu@gmail.com>
2026-04-27 08:16:20 +0800

    Update brand layout in inner client pages

---
+++ update-brand.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const files = ['announcements.html', 'events.html', 'faculty.html', 'gallery.html', 'news.html', 'programs.html'];
+const oldStr = '    <a href="index.html" class="brand">BS Mathematics</a>';
+const newStr = '    <a href="index.html" class="brand">\n      <div class="brand-logo">Σ</div>\n      <span class="brand-text">BS Mathematics</span>\n    </a>';
+for (const file of files) {
+  if (fs.existsSync(file)) {
+    const content = fs.readFileSync(file, 'utf8');
+    if (content.includes(oldStr)) {
+      fs.writeFileSync(file, content.replace(oldStr, newStr), 'utf8');
+      console.log(`Updated ${file}`);
+    }
+  }
+}
