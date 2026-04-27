2caa0ef816821b0f4b9575021f165c3c00887eb1
David Datu Sarmiento <sarmientodaviddatu@gmail.com>
2026-04-27 08:21:08 +0800

    Update brand layout in inner client pages inside client/ folder

--- update-client-brand.js
+++ update-client-brand.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const files = ['announcements.html', 'events.html', 'faculty.html', 'gallery.html', 'news.html', 'programs.html'];
+const path = require('path');
+const files = ['client/announcements.html', 'client/events.html', 'client/faculty.html', 'client/gallery.html', 'client/news.html', 'client/programs.html'];
 const oldStr = '    <a href="index.html" class="brand">BS Mathematics</a>';
 const newStr = '    <a href="index.html" class="brand">\n      <div class="brand-logo">Σ</div>\n      <span class="brand-text">BS Mathematics</span>\n    </a>';
 for (const file of files) {
